abc6ec454c65b75c235b2e47bbff957bb64305ac
sdwfqin <1245366907@qq.com>
2016-07-20 08:40:10 +0800

    RecyclerDemo

---
+++ RecyclerDemo/src/main/java/com/sdwfqin/recyclerdemo/MyData.java
@@ -0,0 +1,39 @@
+package com.sdwfqin.recyclerdemo;
+
+import java.util.UUID;
+
+/**
+ * Created by sdwfqin on 2016/7/7.
+ */
+public class MyData {
+
+    private UUID id;
+    private String title;
+
+    private String text;
+
+    public MyData() {
+
+        this.id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
