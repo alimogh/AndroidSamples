5a1c87397407898c3882f3f5c340819c41c88287
sdwfqin <1245366907@qq.com>
2016-07-29 10:08:00 +0800

    jsoup

--- jsoupdemo/src/main/java/com/sdwfqin/jsoupdemo/MainActivity.java
+++ jsoupdemo/src/main/java/com/sdwfqin/jsoupdemo/MainActivity.java
@@ -32,16 +32,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
 
-                String  html="<p>\n" +
+                String html = "<p>\n" +
                         "\t　　看惯了大漠长河落日，何管那江南缠绵细雨？大漠风沙，落雪梅花，因为久经世味而变得有了些许禅意。然而，不知何时起，我相信前生与今生，一个本不是我能铸就的往事，却一直在我心中徘徊。是因为我是佛前的长明灯，还是我曾经看见凉山上古佛唱经的菩提？<br />\n" +
                         "\t&nbsp;</p>\n" +
                         "<p>\n" +
                         "\t　　大漠漫漫长河，没有江南风荷的淡然；只希望遇一人牧马塞外，看大雪纷飞后红梅缀雪的静染！那时，烧一只陶埙，与那大漠、胡杨共同吹起风沙雪落就已经是最安然的生活&hellip;&hellip;有事感叹：人生像秋风扫落叶般，有时是那么的无情，有时反而给人一种缠绵的感觉！ 看到秋菊凌寒而来，夕阳吐霞而归，那些，那些所谓的伤悲是不是可以放下？塞外牧马，食毡饮雪，虽然所有的一切看似不堪，可对于我来说，或许，那就是最好的去处！ 不入红尘土，何染尘世泥？<br />\n" +
                         "\t&nbsp;</p>\n";
                 Document doc = Jsoup.parse(html);
-                Elements ele=doc.getElementsByTag("p");
-                for(Element e :ele)
-                {
+                Elements ele = doc.getElementsByTag("p");
+                for (Element e : ele) {
                     Log.e(TAG, e.text());
 
                 }
