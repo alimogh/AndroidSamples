3acd9242dc98f3b2cc5a3056b03afc6facf5e8ec
sdwfqin <1245366907@qq.com>
2016-12-09 18:45:50 +0800

    retrofit小例子

--- retrofit/src/main/java/com/sdwfqin/retrofit/activity/MainActivity.java
+++ retrofit/src/main/java/com/sdwfqin/retrofit/activity/MainActivity.java
@@ -13,6 +13,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +22,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 返回Json
         Retrofit mRetrofit = new Retrofit.Builder() //01:获取Retrofit对象
                 .baseUrl(ApiStores.API_SERVER_URL) //02采用链式结构绑定Base url
                 .addConverterFactory(GsonConverterFactory.create()) //使用工厂模式创建Gson的解析器
                 .build();
         //04获取API接口的实现类的实例对象
         ApiStores mApiStores = mRetrofit.create(ApiStores.class);
-        Call<WeatherModel> mModelCall = mApiStores.loadDataByRetrofit("101190201");
+        Call<WeatherModel> mModelCall = mApiStores.loadDataByJson("101190201");
         mModelCall.enqueue(new Callback<WeatherModel>() {
             @Override
             public void onResponse(Call<WeatherModel> call, Response<WeatherModel> response) {
@@ -41,6 +43,24 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<WeatherModel> call, Throwable t) {
+
+            }
+        });
+
+        // 返回字符串
+        Retrofit mRetrofit2 = new Retrofit.Builder() //01:获取Retrofit对象
+                .baseUrl(ApiStores.API_SERVER_URL) //02采用链式结构绑定Base url
+                .addConverterFactory(ScalarsConverterFactory.create()) //使用工厂模式创建字符串解析器
+                .build();
+        ApiStores mApiStores2 = mRetrofit2.create(ApiStores.class);
+        Call<String> mModelCall2 = mApiStores2.loadDataByString("101190201");
+        mModelCall2.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                Log.e("test", "onResponse字符串：" + response.body());
+            }
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
 
             }
         });
--- retrofit/src/main/java/com/sdwfqin/retrofit/api/ApiStores.java
+++ retrofit/src/main/java/com/sdwfqin/retrofit/api/ApiStores.java
@@ -2,8 +2,6 @@ package com.sdwfqin.retrofit.api;
 
 import com.sdwfqin.retrofit.model.WeatherModel;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -17,5 +15,8 @@ public interface ApiStores {
     String API_SERVER_URL = "http://www.weather.com.cn/";
     //加载天气
     @GET("adat/sk/{cityId}.html")
-    Call<WeatherModel> loadDataByRetrofit(@Path("cityId") String cityId);
+    Call<WeatherModel> loadDataByJson(@Path("cityId") String cityId);
+    //返回字符串
+    @GET("adat/sk/{cityId}.html")
+    Call<String> loadDataByString(@Path("cityId") String cityId);
 }
