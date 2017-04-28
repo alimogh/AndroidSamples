f0dbdee8573e45f3447a46d99701fd5a35bd8539
sdwfqin <1245366907@qq.com>
2017-04-28 16:31:11 +0800

    canvas绘图

--- app/src/main/java/com/sdwfqin/sample/canvas/CanvasView.java
+++ app/src/main/java/com/sdwfqin/sample/canvas/CanvasView.java
@@ -21,7 +21,6 @@ import com.sdwfqin.sample.R;
 /**
  * Created by sdwfqin on 2017/4/27.
  */
-
 public class CanvasView extends LinearLayout {
 
     private static final String TAG = "CanvasView";
@@ -44,28 +43,26 @@ public class CanvasView extends LinearLayout {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
-        Log.e(TAG, "getRawX: " + event.getRawX());
-        Log.e(TAG, "getRawY: " + event.getRawY());
         Log.e(TAG, "getX: " + event.getX());
         Log.e(TAG, "getY: " + event.getY());
 
         return true;
     }
 
+    // TODO: Canvas绘图
     // 如果onDraw方法不调用，需要在构造方法中添加setWillNotDraw(false);
     // 解释：设置view是否更改，如果开发者用自定义的view，重写ondraw（）应该将调用此方法设置为false，这样程序会调用自定义的布局。
     // 参考文章链接：http://blog.csdn.net/look85/article/details/8442675
     @Override
     protected void onDraw(Canvas canvas) {
 
+        Log.e(TAG, "onDraw: " + getWidth());
+        Log.e(TAG, "onDraw: " + getHeight());
+
         Log.e(TAG, "onDraw: " + "画图");
 
         super.onDraw(canvas);
 
-        /*
-         * 方法 说明 drawRect 绘制矩形 drawCircle 绘制圆形 drawOval 绘制椭圆 drawPath 绘制任意多边形
-         * drawLine 绘制直线 drawPoin 绘制点
-         */
         // 创建画笔
         Paint p = new Paint();
         p.setColor(Color.RED);// 设置红色
@@ -73,100 +70,158 @@ public class CanvasView extends LinearLayout {
         // 设置绘制的文本大小
         p.setTextSize(60);
         // x,y方向的值均为相对于当前布局
-        // x水平方向的七点，y代表竖直方向的底部
-        canvas.drawText("画圆：", 100, 100, p);// 画文本
-        // x,y表示圆心的位置，r半径
-        canvas.drawCircle(350, 80, 50, p);// 小圆
-        p.setAntiAlias(true);// 设置画笔的锯齿效果。 true是去除，大家一看效果就明白了
-        canvas.drawCircle(550, 80, 100, p);// 大圆
-
-//        canvas.drawText("画线及弧线：", 10, 60, p);
-//        p.setColor(Color.GREEN);// 设置绿色
-//        canvas.drawLine(60, 40, 100, 40, p);// 画线
-//        canvas.drawLine(110, 40, 190, 80, p);// 斜线
-//        //画笑脸弧线
-//        p.setStyle(Paint.Style.STROKE);//设置空心
-//        RectF oval1 = new RectF(150, 20, 180, 40);
-//        canvas.drawArc(oval1, 180, 180, false, p);//小弧形
-//        oval1.set(190, 20, 220, 40);
-//        canvas.drawArc(oval1, 180, 180, false, p);//小弧形
-//        oval1.set(160, 30, 210, 60);
-//        canvas.drawArc(oval1, 0, 180, false, p);//小弧形
-//
-//        canvas.drawText("画矩形：", 10, 80, p);
-//        p.setColor(Color.GRAY);// 设置灰色
-//        p.setStyle(Paint.Style.FILL);//设置填满
-//        canvas.drawRect(60, 60, 80, 80, p);// 正方形
-//        canvas.drawRect(60, 90, 160, 100, p);// 长方形
-
-//        canvas.drawText("画扇形和椭圆:", 10, 120, p);
-//        /* 设置渐变色 这个正方形的颜色是改变的 */
-//        Shader mShader = new LinearGradient(0, 0, 100, 100,
-//                new int[]{Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW,
-//                        Color.LTGRAY}, null, Shader.TileMode.REPEAT); // 一个材质,打造出一个线性梯度沿著一条线。
-//        p.setShader(mShader);
-//        // p.setColor(Color.BLUE);
-//        RectF oval2 = new RectF(60, 100, 200, 240);// 设置个新的长方形，扫描测量
-//        canvas.drawArc(oval2, 200, 130, true, p);
-//        // 画弧，第一个参数是RectF：该类是第二个参数是角度的开始，第三个参数是多少度，第四个参数是真的时候画扇形，是假的时候画弧线
-//        //画椭圆，把oval改一下
-//        oval2.set(210, 100, 250, 130);
-//        canvas.drawOval(oval2, p);
-//
-//        canvas.drawText("画三角形：", 10, 200, p);
-//        // 绘制这个三角形,你可以绘制任意多边形
-//        Path path = new Path();
-//        path.moveTo(80, 200);// 此点为多边形的起点
-//        path.lineTo(120, 250);
-//        path.lineTo(80, 250);
-//        path.close(); // 使这些点构成封闭的多边形
-//        canvas.drawPath(path, p);
-//
-//        // 你可以绘制很多任意多边形，比如下面画六连形
-//        p.reset();//重置
-//        p.setColor(Color.LTGRAY);
-//        p.setStyle(Paint.Style.STROKE);//设置空心
-//        Path path1 = new Path();
-//        path1.moveTo(180, 200);
-//        path1.lineTo(200, 200);
-//        path1.lineTo(210, 210);
-//        path1.lineTo(200, 220);
-//        path1.lineTo(180, 220);
-//        path1.lineTo(170, 210);
-//        path1.close();//封闭
-//        canvas.drawPath(path1, p);
-        /*
-         * Path类封装复合(多轮廓几何图形的路径
-         * 由直线段*、二次曲线,和三次方曲线，也可画以油画。drawPath(路径、油漆),要么已填充的或抚摸
-         * (基于油漆的风格),或者可以用于剪断或画画的文本在路径。
+        /**
+         * @param text  文本
+         * @param x     水平方向起点
+         * @param y     竖直方向的文字底部
+         * @param paint 画笔
          */
+        canvas.drawText("画圆：", 50, 100, p);// 画文本
+        // x,y表示圆心的位置，r半径
+        /**
+         * 画圆
+         * @param cx     圆心x坐标
+         * @param cy     圆心y坐标
+         * @param radius 半径
+         * @param paint  画笔
+         */
+        canvas.drawCircle(300, 80, 50, p);// 小圆
+        // 设置画笔的锯齿效果。true是去除
+        p.setAntiAlias(true);
+        canvas.drawCircle(500, 80, 100, p);// 大圆
 
-//        //画圆角矩形
-//        p.setStyle(Paint.Style.FILL);//充满
-//        p.setColor(Color.LTGRAY);
-//        p.setAntiAlias(true);// 设置画笔的锯齿效果
-//        canvas.drawText("画圆角矩形:", 10, 260, p);
-//        RectF oval3 = new RectF(80, 260, 200, 300);// 设置个新的长方形
-//        canvas.drawRoundRect(oval3, 20, 15, p);//第二个参数是x半径，第三个参数是y半径
+        canvas.drawText("画线及弧线：", 50, 250, p);
+        p.setColor(Color.GREEN);// 设置绿色
+        p.setStrokeWidth(10);//设置线条宽度
+        /**
+         * 画线条
+         * @param startX 起点x坐标
+         * @param startX 起点y坐标
+         * @param stopX 终点x坐标
+         * @param stopY 终点y坐标
+         * @param paint  画笔
+         */
+        canvas.drawLine(500, 225, 700, 225, p);// 画线
+        canvas.drawLine(650, 110, 800, 250, p);// 斜线
 
-//        //画贝塞尔曲线
-//        canvas.drawText("画贝塞尔曲线:", 10, 310, p);
-//        p.reset();
-//        p.setStyle(Paint.Style.STROKE);
-//        p.setColor(Color.GREEN);
-//        Path path2 = new Path();
-//        path2.moveTo(100, 320);//设置Path的起点
-//        path2.quadTo(150, 310, 170, 400); //设置贝塞尔曲线的控制点坐标和终点坐标
-//        canvas.drawPath(path2, p);//画出贝塞尔曲线
-//
-//        //画点
-//        p.setStyle(Paint.Style.FILL);
-//        canvas.drawText("画点：", 10, 390, p);
-//        canvas.drawPoint(60, 390, p);//画一个点
-//        canvas.drawPoints(new float[]{60, 400, 65, 400, 70, 400}, p);//画多个点
-//
-//        //画图片，就是贴图
-//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
-//        canvas.drawBitmap(bitmap, 250, 360, p);
+        /**
+         * 画笑脸方法：创建一个RectF区域（这个区域仅容纳第一笔）画第一笔，第一笔绘制完成修改RectF区域画第二笔，依次类推
+         */
+        //设置空心
+        p.setStyle(Paint.Style.STROKE);
+        /**
+         * 表示一块矩形区域(left <= right and top <= bottom)
+         * @param left   左上角x
+         * @param top    左上角y
+         * @param right  右下角x
+         * @param bottom 右下角y
+         */
+        RectF oval1 = new RectF(800, 50, 900, 100);
+        /**
+         * oval：圆弧所在的RectF对象。
+         * startAngle：圆弧的起始角度。
+         * sweepAngle：圆弧的角度。
+         * useCenter：是否显示半径连线，true表示显示圆弧与圆心的半径连线，false表示不显示。
+         * paint：绘制时所使用的画笔。
+         */
+        canvas.drawArc(oval1, 180, 180, false, p);//小弧形
+        /**
+         * 设置RectF布局，参数与创建RectF相同
+         */
+        oval1.set(950, 50, 1050, 100);
+        canvas.drawArc(oval1, 180, 180, false, p);//小弧形
+        oval1.set(850, 100, 1000, 200);
+        canvas.drawArc(oval1, 0, 180, false, p);//小弧形
+
+        p.setStyle(Paint.Style.FILL);//设置填满（默认）
+        p.setColor(Color.GRAY);// 设置灰色
+        canvas.drawText("画矩形：", 50, 400, p);
+        /**
+         * 绘制矩形（两点确定位置，左上角与右下角）
+         * @param left   左上角x
+         * @param top    左上角y
+         * @param right  右下角x
+         * @param bottom 右下角y
+         * @param paint：绘制时所使用的画笔。
+         */
+        canvas.drawRect(300, 350, 500, 400, p);// 正方形
+        canvas.drawRect(600, 300, 700, 400, p);// 长方形
+
+        /**
+         * Shader类专门用来渲染图像以及一些几何图形
+         * LinearGradient是Shader的子类，用来进行梯度渲染
+         * Shader类的使用，需要先构建Shader对象，然后通过Paint的setShader方法设置渲染对象，
+         * 然后设置渲染对象，然后再绘制时使用这个Paint对象即可。
+         *
+         * @param x0 起始的x坐标
+         * @param y0 起始的y坐标
+         * @param x1 结束的x坐标
+         * @param y1 结束的y坐标
+         * @param  colors 颜色数组
+         * @param  positions 这个也是一个数组用来指定颜色数组的相对位置 如果为null 就沿坡度线均匀分布
+         * @param  tile 渲染模式
+         */
+        Shader mShader = new LinearGradient(0, 0, 100, 100,
+                new int[]{Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW,
+                        Color.LTGRAY}, null, Shader.TileMode.REPEAT);
+        p.setShader(mShader);
+        canvas.drawText("画扇形和椭圆:", 50, 550, p);
+        RectF oval2 = new RectF(450, 450, 650, 650);// 设置个新的长方形，扫描测量
+        canvas.drawArc(oval2, 200, 130, true, p);
+        //画椭圆
+        oval2.set(650, 450, 900, 650);
+        canvas.drawOval(oval2, p);
+        canvas.drawRect(0, 650, 1080, 700, p);// 长方形
+        // 取消Shader
+        p.setShader(null);
+        // or重置画笔
+        // p.reset();
+        p.setColor(Color.BLUE);
+
+        canvas.drawText("画三角形：", 50, 800, p);
+        // 绘制这个三角形,你可以绘制任意多边形
+        // path：多边形封装类
+        Path path = new Path();
+        path.moveTo(500, 750);// 此点为多边形的起点
+        path.lineTo(400, 850);
+        path.lineTo(600, 850);
+        path.close(); // 使这些点构成封闭的多边形
+        canvas.drawPath(path, p);
+
+        //画圆角矩形
+        canvas.drawText("画圆角矩形:", 50, 950, p);
+        RectF oval3 = new RectF(500, 880, 700, 1000);// 设置个新的长方形
+        //第二个参数是x半径，第三个参数是y半径
+        canvas.drawRoundRect(oval3, 15, 15, p);
+
+        p.setColor(Color.GREEN);
+        //画贝塞尔曲线
+        canvas.drawText("画贝塞尔曲线:", 50, 1100, p);
+        p.setStyle(Paint.Style.STROKE);
+        Path path2 = new Path();
+        path2.moveTo(500, 1050);//设置Path的起点
+        path2.quadTo(600, 950, 700, 1050); //设置贝塞尔曲线的控制点坐标和终点坐标
+        path2.quadTo(800, 1150, 900, 1050); //设置贝塞尔曲线的控制点坐标和终点坐标
+        canvas.drawPath(path2, p);//画出贝塞尔曲线
+
+        //画点
+        p.setStyle(Paint.Style.FILL);
+        canvas.drawText("画点：", 50, 1250, p);
+        /**
+         * x,y坐标
+         */
+        canvas.drawPoint(500, 1200, p);//画一个点
+        canvas.drawPoints(new float[]{600, 1200, 650, 1250, 700, 1200}, p);//画多个点
+
+        //画图片，就是贴图
+        canvas.drawText("画图片：", 50, 1400, p);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        /**
+         * @param bitmap The bitmap to be drawn
+         * @param left   左上角x坐标
+         * @param top    左上角y坐标
+         * @param paint  画笔
+         */
+        canvas.drawBitmap(bitmap, 360, 1300, p);
     }
 }
