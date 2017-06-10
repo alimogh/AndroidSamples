1b508ea087f1e4514e5b022fe5e59ffdab1ce160
sdwfqin <1245366907@qq.com>
2017-06-10 13:30:51 +0800

    添加注释

--- drrmvp/src/main/java/com/sdwfqin/drrmvp/model/http/RetrofitHelper.java
+++ drrmvp/src/main/java/com/sdwfqin/drrmvp/model/http/RetrofitHelper.java
@@ -11,11 +11,24 @@ public class RetrofitHelper implements HttpHelper {
 
     private TestApi mTestApiService;
 
+    /**
+     * 注入
+     * <p>
+     * 详见：HttpModule -> provideZhihuService
+     *
+     * @param testApiService TestApi接口的实现类的实例对象
+     */
     @Inject
     public RetrofitHelper(TestApi testApiService) {
         this.mTestApiService = testApiService;
     }
 
+    /**
+     * 调用接口中的业务方法
+     *
+     * @param cityId
+     * @return
+     */
     @Override
     public Flowable<WeatherBean> fetchWeather(String cityId) {
         return mTestApiService.getWeather(cityId);
